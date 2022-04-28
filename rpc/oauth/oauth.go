3a223b913b28e80e4e80b56f01371cbf3666e29d
yangwb1123 <1832227346@qq.com>
2022-04-28 08:10:39 +0800

    feat(rpc): add `GetOauthCode`, `Login`, `Logout`

--- rpc/oauth/oauth.go
+++ rpc/oauth/oauth.go
@@ -35,4 +35,57 @@ func (s *OauthRpcServer) GetGrantCode(ctx context.Context, in *pb.GetGrantCodeRe
 	return &pb.GetGrantCodeReply{GrantCode: utils.ToRpcStruct(reply.OkWithData(grantedCode))}, nil
 }
 
+func (s *OauthRpcServer) GetOauthCode(ctx context.Context, in *pb.GetOauthCodeRequest) (*pb.GetOauthCodeReply, error) {
+	grantType := in.GetGrantType()
+	clientId := in.GetClientId()
+	clientSecret := in.GetClientSecret()
+	code := in.GetCode()
+	redirectUri := in.GetRedirectUri()
 
+	if grantType != "authorization_code" {
+		return &pb.GetOauthCodeReply{OauthInfo: utils.ToRpcStruct(reply.FailWithDetail(nil, "grant type error"))}, nil
+	}
+	if code == "" {
+		return &pb.GetOauthCodeReply{OauthInfo: utils.ToRpcStruct(reply.FailWithDetail(nil, "empty code"))}, nil
+	}
+	accessToken, refreshToken, user, err := oauthService.DoGenerateOauthCode(clientId, clientSecret, code, redirectUri)
+	if err != nil {
+		return &pb.GetOauthCodeReply{OauthInfo: utils.ToRpcStruct(reply.FailWithDetail(nil, err.Error()))}, nil
+	}
+	info := map[string]interface{}{
+		"accessToken":  accessToken,
+		"refreshToken": refreshToken,
+		"user":         user,
+	}
+	return &pb.GetOauthCodeReply{OauthInfo: utils.ToRpcStruct(reply.OkWithData(info))}, nil
+}
+
+func (s *OauthRpcServer) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply, error) {
+	username := in.GetUsername()
+	password := in.GetPassword()
+	loginType := in.GetLoginType()
+	clientId := in.GetClientId()
+
+	accessToken, refreshToken, user, err := oauthService.DoOauthLogin(username, password, loginType, clientId)
+
+	if err != nil {
+		return &pb.LoginReply{OauthInfo: utils.ToRpcStruct(reply.FailWithDetail(nil, err.Error()))}, nil
+	}
+	info := map[string]interface{}{
+		"accessToken":  accessToken,
+		"refreshToken": refreshToken,
+		"user":         user,
+	}
+	return &pb.LoginReply{OauthInfo: utils.ToRpcStruct(reply.OkWithData(info))}, nil
+}
+
+func (s OauthRpcServer) Logout(ctx context.Context, in *pb.LogoutRequest) (*pb.LogoutReply, error) {
+	token := in.GetAccessToken()
+	j := utils.NewJWT()
+	claims, _ := j.ParseToken(token)
+	affected, err := oauthService.DeleteAccessTokenFromRedis(claims.UserId)
+	if err == nil && affected > 0 {
+		return &pb.LogoutReply{LogoutResult: utils.ToRpcStruct(reply.Ok())}, nil
+	}
+	return &pb.LogoutReply{LogoutResult: utils.ToRpcStruct(reply.Fail())}, nil
+}
