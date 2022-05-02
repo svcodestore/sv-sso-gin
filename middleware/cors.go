ab2bea9790fe71f05c96ada39f0a1a2d5c535083
yangwb1123 <1832227346@qq.com>
2022-05-02 11:16:21 +0800

    fix(cors):no header when method is option

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,15 +10,16 @@ func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
 
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Max-Age", "86400")
+
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
 			return
 		}
 
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "*")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Max-Age", "86400")
 		c.Next()
 	}
 }
