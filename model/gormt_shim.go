607502a6f8c5f34960cb5a8787f7836064227e53
yangwb1123 <1832227346@qq.com>
2022-06-15 14:46:39 +0800

    refactor(model): follow sql

--- model/gormt_shim.go
+++ model/gormt_shim.go
@@ -1,26 +1,30 @@
 package model
 
 import (
+	"gorm.io/datatypes"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
 
 type UsersWithoutModInfo struct {
-	ID        string    `gorm:"primaryKey;column:id;type:bigint;not null" json:"id"`
-	UUID      uuid.UUID `gorm:"unique;index:user_uuid_index;column:uuid;type:binary(16);not null" json:"uuid"`
-	LoginID   string    `gorm:"unique;column:login_id;type:varchar(16);not null" json:"loginId"`
-	Password  string    `gorm:"column:password;type:varchar(1024)" json:"-"`
-	Name      string    `gorm:"column:name;type:varchar(32)" json:"name"`
-	Alias     string    `gorm:"column:alias;type:varchar(32)" json:"alias"`
-	Phone     string    `gorm:"column:phone;type:varchar(16)" json:"phone"`
-	Email     string    `gorm:"column:email;type:varchar(1024)" json:"email"`
-	Lang      string    `gorm:"column:lang;type:char(5);default:zh_CN" json:"lang"`
-	Status    uint8     `gorm:"column:status;type:tinyint(1);not null;default:1" json:"status"`
-	CreatedAt time.Time `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"-"`
-	CreatedBy string    `gorm:"column:created_by;type:bigint;not null" json:"-"`
-	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"-"`
-	UpdatedBy string    `gorm:"column:updated_by;type:bigint;not null" json:"-"`
+	ID        string         `gorm:"primaryKey;column:id;type:bigint;not null" json:"id"`
+	UUID      uuid.UUID      `gorm:"unique;index:user_uuid_index;column:uuid;type:binary(16);not null" json:"uuid"`
+	LoginID   string         `gorm:"unique;column:login_id;type:varchar(16);not null" json:"loginId"`
+	Password  string         `gorm:"column:password;type:varchar(1024)" json:"-"`
+	Name      string         `gorm:"column:name;type:varchar(32)" json:"name"`
+	Avatar    string         `gorm:"column:avatar;type:varchar(1024)" json:"avatar"`
+	Alias     string         `gorm:"column:alias;type:varchar(32)" json:"alias"`
+	Phone     string         `gorm:"column:phone;type:varchar(16)" json:"phone"`
+	Email     string         `gorm:"column:email;type:varchar(1024)" json:"email"`
+	Gender    uint8          `gorm:"column:gender;type:tinyint unsigned;not null;default:2" json:"gender"` // 0 is female, 1 is male, 2 in unknown
+	Lang      string         `gorm:"column:lang;type:char(5);default:zh_CN" json:"lang"`
+	HomePath  datatypes.JSON `gorm:"column:home_path;type:json" json:"homePath"`
+	Status    uint8          `gorm:"column:status;type:tinyint(1);not null;default:1" json:"status"`
+	CreatedAt time.Time      `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"-"`
+	CreatedBy string         `gorm:"column:created_by;type:bigint;not null" json:"-"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"-"`
+	UpdatedBy string         `gorm:"column:updated_by;type:bigint;not null" json:"-"`
 }
 
 type Users struct {
@@ -29,10 +33,13 @@ type Users struct {
 	LoginID       string              `gorm:"unique;column:login_id;type:varchar(16);not null" json:"loginId"`
 	Password      string              `gorm:"column:password;type:varchar(1024)" json:"password"`
 	Name          string              `gorm:"column:name;type:varchar(32)" json:"name"`
+	Avatar        string              `gorm:"column:avatar;type:varchar(1024)" json:"avatar"`
 	Alias         string              `gorm:"column:alias;type:varchar(32)" json:"alias"`
 	Phone         string              `gorm:"column:phone;type:varchar(16)" json:"phone"`
 	Email         string              `gorm:"column:email;type:varchar(1024)" json:"email"`
+	Gender        uint8               `gorm:"column:gender;type:tinyint unsigned;not null;default:2" json:"gender"` // 0 is female, 1 is male, 2 in unknown
 	Lang          string              `gorm:"column:lang;type:char(5);default:zh_CN" json:"lang"`
+	HomePath      datatypes.JSON      `gorm:"column:home_path;type:json" json:"homePath"`
 	Status        uint8               `gorm:"column:status;type:tinyint(1);not null;default:1" json:"status"`
 	CreatedAt     time.Time           `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"createdAt"`
 	CreatedBy     string              `gorm:"column:created_by;type:bigint;not null" json:"-"`
@@ -43,20 +50,23 @@ type Users struct {
 }
 
 type UsersToSave struct {
-	ID        string    `gorm:"primaryKey;column:id;type:bigint;not null" json:"id"`
-	UUID      []byte    `gorm:"unique;index:user_uuid_index;column:uuid;type:binary(16);not null" json:"uuid"`
-	LoginID   string    `gorm:"unique;column:login_id;type:varchar(16);not null" json:"loginId"`
-	Password  string    `gorm:"column:password;type:varchar(1024)" json:"password"`
-	Name      string    `gorm:"column:name;type:varchar(32)" json:"name"`
-	Alias     string    `gorm:"column:alias;type:varchar(32)" json:"alias"`
-	Phone     string    `gorm:"column:phone;type:varchar(16)" json:"phone"`
-	Email     string    `gorm:"column:email;type:varchar(1024)" json:"email"`
-	Lang      string    `gorm:"column:lang;type:char(5);default:zh_CN" json:"lang"`
-	Status    uint8     `gorm:"column:status;type:tinyint(1);not null;default:1" json:"status"`
-	CreatedAt time.Time `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"createdAt"`
-	CreatedBy string    `gorm:"column:created_by;type:bigint;not null" json:"createdBy"`
-	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"updatedAt"`
-	UpdatedBy string    `gorm:"column:updated_by;type:bigint;not null" json:"updatedBy"`
+	ID        string         `gorm:"primaryKey;column:id;type:bigint;not null" json:"id"`
+	UUID      []byte         `gorm:"unique;index:user_uuid_index;column:uuid;type:binary(16);not null" json:"uuid"`
+	LoginID   string         `gorm:"unique;column:login_id;type:varchar(16);not null" json:"loginId"`
+	Password  string         `gorm:"column:password;type:varchar(1024)" json:"password"`
+	Name      string         `gorm:"column:name;type:varchar(32)" json:"name"`
+	Avatar    string         `gorm:"column:avatar;type:varchar(1024)" json:"avatar"`
+	Alias     string         `gorm:"column:alias;type:varchar(32)" json:"alias"`
+	Phone     string         `gorm:"column:phone;type:varchar(16)" json:"phone"`
+	Email     string         `gorm:"column:email;type:varchar(1024)" json:"email"`
+	Gender    uint8          `gorm:"column:gender;type:tinyint unsigned;not null;default:2" json:"gender"` // 0 is female, 1 is male, 2 in unknown
+	Lang      string         `gorm:"column:lang;type:char(5);default:zh_CN" json:"lang"`
+	HomePath  datatypes.JSON `gorm:"column:home_path;type:json" json:"homePath"`
+	Status    uint8          `gorm:"column:status;type:tinyint(1);not null;default:1" json:"status"`
+	CreatedAt time.Time      `gorm:"column:created_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"createdAt"`
+	CreatedBy string         `gorm:"column:created_by;type:bigint;not null" json:"createdBy"`
+	UpdatedAt time.Time      `gorm:"column:updated_at;type:datetime(6);not null;default:CURRENT_TIMESTAMP(6)" json:"updatedAt"`
+	UpdatedBy string         `gorm:"column:updated_by;type:bigint;not null" json:"updatedBy"`
 }
 
 func (m *UsersToSave) TableName() string {
